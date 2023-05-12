389423b8df1a6dd7d9d2f5b7fcbfed988da8c95d
Kuhn, Matthew L <mlkuhn@tva.gov>
2023-05-12 13:22:36 -0400

    these were never going to stay in the environment anyway.... It's just a POC!! :)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,4 @@
 export const environment = {
-  AWS_ACCESS_KEY_ID: "AKIA55GCLI65LIBTCO7W",
-  AWS_SECRET_ACCESS_KEY: "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",
-  AWS_REGION: "us-east-1",
-  AWS_BUCKET: "dynamic-form-renderer-forms",
-  AWS_BUCKET_ENTRIES: "dynamic-form-renderer-entries",
   FORMEDIT_API: "https://af8cubf90j.execute-api.us-east-1.amazonaws.com/default/FormEdit",
   FORMENTRY_API: "https://t5q463fi52.execute-api.us-east-1.amazonaws.com/default/FormEntry",
 };
