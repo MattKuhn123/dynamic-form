e5ac458321d72c92214ebf9fc73c2e0ae07f49e0
MattKuhn123 <102084893+MattKuhn123@users.noreply.github.com>
2024-11-22 13:18:27 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  FORMEDIT_API: "https://af8cubf90j.execute-api.us-east-1.amazonaws.com/default/FormEdit",
-  FORMENTRY_API: "https://t5q463fi52.execute-api.us-east-1.amazonaws.com/default/FormEntry",
+  FORMEDIT_API: "",
+  FORMENTRY_API: "",
 };
